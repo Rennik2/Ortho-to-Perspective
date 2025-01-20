f91ec26ef20cc43d267fcd21385d165ffe8a0a2c
Seamus-C <happyphizzy@gmail.com>
2025-01-19 16:56:58 -0800

    try again to get the scale to work
    I started from what I had before

--- Assets/Scripts/Move vertex.cs
+++ Assets/Scripts/Move vertex.cs
@@ -127,4 +127,36 @@ public class MoveVertex : MonoBehaviour
 
         return intersectionPoint;
     }
+    
+    private Vector3 VertFromLocalToWorldSpace(GameObject gameObject, Vector3 vert)
+    {
+        Matrix4x4 transformationMatrix = gameObject.transform.localToWorldMatrix;
+        // Might want to see if MultiplyPoint3x4 works (it would be faster)
+        Vector3 vert_WS = transformationMatrix.MultiplyPoint(vert);
+        return vert_WS;
+    }
+    private Vector3[] VerticesFromLocalToWorldSpace(GameObject gameObject, Vector3[] vertices)
+    {
+        Vector3[] vertices_WS = new Vector3[vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices_WS[i] = VertFromLocalToWorldSpace(gameObject, vertices[i]);
+        }
+        return vertices_WS;
+    }
+    private Vector3 VertFromWorldToLocalSpace(GameObject gameObject, Vector3 vert)
+    {
+        Matrix4x4 transformationMatrix = gameObject.transform.worldToLocalMatrix;
+        Vector3 vert_LS = transformationMatrix.MultiplyPoint(vert);
+        return vert_LS;
+    }
+    private Vector3[] VerticesFromWorldToLocalSpace(GameObject gameObject, Vector3[] vertices)
+    {
+        Vector3[] vertices_LS = new Vector3[vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices_LS[i] = VertFromWorldToLocalSpace(gameObject, vertices[i]);
+        }
+        return vertices_LS;
+    }
 }
